d04295c3d6f6356669bedc7b823cd2b4a7a1cab9
IFM-web <ifmsoftech@gmail.com>
2025-03-13 10:55:19 +0530

    add

---
+++ GuardTour/AuthFilter/AuthenticationFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuardTour.AuthFilter
+{
+  
+        public class AuthenticationFilter : ActionFilterAttribute
+        {
+            public override void OnActionExecuting(ActionExecutingContext context)
+            {
+                var userId = context.HttpContext.Session.GetString("UserId");
+
+                if (string.IsNullOrEmpty(userId))
+                {
+                    var controller = (Controller)context.Controller;
+                    context.Result = controller.RedirectToAction("Login", "Home");
+                }
+            }
+        }
+    
+}
+
