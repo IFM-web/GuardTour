8ba06f87e44a8fda3f6981285f75781f646c1525
IFM-web <ifmsoftech@gmail.com>
2025-05-12 10:36:48 +0530

    add

---
+++ GuardTour/Controllers/APIDOCController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuardTour.Controllers
+{
+    public class APIDOCController : Controller
+    {
+        public IActionResult APIDocs()
+        {
+
+            return View();
+        }
+
+		public IActionResult ApiDocMaster()
+		{
+			return View();
+		}
+
+        
+	}
+}
