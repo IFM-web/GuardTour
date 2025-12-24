eb10fac434b08810389f0b846cf3c97003c4c867
Nadeem-Ali-Dot <Nadeemalimachhr2002@gmail.com>
2025-12-24 17:30:22 +0530

    add all

---
+++ Models/TourReport.cs
@@ -0,0 +1,12 @@
+﻿namespace GuardTour.Models
+{
+    public class TourReport
+    {
+        public int Id { get; set; }
+        public int TourId { get; set; }
+        public DateTime ReportDate { get; set; }
+        public string Summary { get; set; }
+        public string Details { get; set; }
+
+    }
+}
